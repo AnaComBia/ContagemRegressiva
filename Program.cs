6403a90893a185d83d6d43a5082763d37017af38
AnaComBia <anabeatrizetorres@gmail.com>
2021-08-21 13:53:29 -0300

    Finaliza o Programa Contagem

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ContagemRegressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadKey();
+            Console.WriteLine("Preparar...");
+            Console.ReadKey();
+            Console.WriteLine("Apontar...");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.ReadKey();
+            Console.WriteLine("FOGO!");
+            Console.ResetColor();
+
+        }
+    }
+}
